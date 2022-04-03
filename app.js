b1ab5776289d16c62af91a10e3d9ae4d3b3e7825
tejeshreddy17 <93307431+tejeshreddy17@users.noreply.github.com>
2022-04-03 21:44:09 +0530

    final

--- app.js
+++ app.js
@@ -123,7 +123,10 @@ app.listen(port, function() {
     console.log(`Server started successfully at ${port}`);
    });
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get("/", async (request, response) => {
     try {
